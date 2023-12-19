d969337a98e8174b49c5bfe95121c8c9add5c343
Marcin Niemirski <marcin.niemirski@tsh.io>
2023-12-19 17:24:24 +0100

    add delete chat route

---
+++ app/api/chat/delete/route.ts
@@ -0,0 +1,48 @@
+import { authOptions } from "@/auth/options";
+import { adminDb } from "@/firebase-admin";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+export async function DELETE(req: Request) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json(
+      {
+        success: false,
+      },
+      { status: 401 }
+    );
+  }
+
+  const { chatId } = await req.json();
+  const ref = adminDb.collection("chats").doc(chatId);
+  const bulkWriter = adminDb.bulkWriter();
+  const MAX_RETRY_ATTEMPTS = 5;
+  bulkWriter.onWriteError((error) => {
+    if (error.failedAttempts < MAX_RETRY_ATTEMPTS) {
+      return true;
+    } else {
+      console.log("Faild write at document: ", error.documentRef.path);
+      return false;
+    }
+  });
+
+  try {
+    await adminDb.recursiveDelete(ref, bulkWriter);
+    return NextResponse.json(
+      {
+        success: true,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Promise rejected :", error);
+
+    return NextResponse.json(
+      {
+        success: false,
+      },
+      { status: 500 }
+    );
+  }
+}
